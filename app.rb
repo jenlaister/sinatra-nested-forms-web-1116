31bf4c4776d5d3907a360486590896938e2cd51b
Jenny Lai <jenny.lai@flatironschool.com>
2016-12-18 15:33:17 -0500

    ARGHHHH

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+  get '/' do
+    erb :root
+  end
+
+  get '/new' do
+    erb (:'pirates/new')
+  end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate][:name],params[:pirate][:weight],params[:pirate][:height])
+      @ships = Ship.all
+      params[:pirate][:ships].each {|ship| Ship.new(name: ship[:name], type: ship[:type], booty: ship[:booty])}
+      erb (:'pirates/show')
+    end
 
   end
 end
